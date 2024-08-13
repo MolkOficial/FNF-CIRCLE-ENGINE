6ae4e73a3d63666c8088db3b55109305fc084304
Molk <97996807+MolkOficial@users.noreply.github.com>
2024-08-12 20:06:18 -0400

    Add files via upload

---
+++ project.lua
@@ -0,0 +1,28 @@
+return {
+	DEBUG_MODE = true,
+	splashScreen = true,
+
+	title = "FNF Circle Engine",
+	file = "FNF-CIRCLE-ENGINE",
+	icon = "art/icon.png",
+	version = "Alpha 2",
+	package = "com.molk.circle.engine",
+	width = 1280,
+	height = 720,
+	company = "Molk",
+
+	flags = {
+		CheckForUpdates = false,
+
+		InitialAutoFocus = true,
+		InitialParallelUpdate = true,
+		InitialAsyncInput = false,
+
+		LoxelInitWindow = false,
+		LoxelForceRenderCameraComplex = false,
+		LoxelDisableRenderCameraComplex = false,
+		LoxelDisableScissorOnRenderCameraSimple = false,
+		LoxelDefaultClipCamera = true,
+		LoxelShowPrintsInScreen = false
+	}
+}
